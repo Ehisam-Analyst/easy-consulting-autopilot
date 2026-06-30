eb39c2973f997e6eaba6a986fc2167dd14735db9
Samuel <samuelamienghemen@gmail.com>
2026-06-30 14:44:54 +0100

    update config

--- config.py
+++ config.py
@@ -4,5 +4,6 @@ BASE_DIR = Path(__file__).parent
 
 TEMPLATE_DIR = BASE_DIR / "templates"
 OUTPUT_DIR = BASE_DIR / "output"
+OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
 
 TEMPLATE_FILE = TEMPLATE_DIR / "RegTech365_Proposal_Template.docx"
