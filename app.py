29be75a4220296241683521942b71d6211ee719d
Samuel <samuelamienghemen@gmail.com>
2026-06-30 15:00:32 +0100

    updated app and proposal to automatically download document

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
 from models import Lead
 from services.proposal_services import create_proposal
 from services.error_handler import handle_error
@@ -7,18 +8,19 @@ app = FastAPI()
 
 @app.post("/generate-proposal")
 def generate(lead: Lead):
-        print("Request received")
+    print("Request received")
 
-        lead_data = lead.dict()
-        print("Lead data:", lead_data)
+    lead_data = lead.dict()
+    print("Lead data:", lead_data)
 
-        filename = create_proposal(lead_data)
-        print("Generated:", filename)
+    filename = create_proposal(lead_data)
+    print("Generated:", filename)
 
-        return {
-            "status": "success",
-            "filename": filename
-        }
+    return FileResponse(
+        path=str(filename),
+        filename=filename.name,
+        media_type="application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+    )
 
 
 
--- proposal.py
+++ proposal.py
@@ -1,9 +1,11 @@
 
 from docxtpl import DocxTemplate
 from datetime import date
+from datetime import datetime
 from config import TEMPLATE_FILE, OUTPUT_DIR
 
 template = DocxTemplate(str(TEMPLATE_FILE))
+timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
 
 
 today = date.today()
@@ -16,11 +18,7 @@ def generate_proposal(lead):
         "date": today,
     }
     template.render(context)
-    filename = OUTPUT_DIR / f"{lead['company']}_Proposal.docx"   
+    filename = OUTPUT_DIR / f"{lead['company']}_{timestamp}_Proposal.docx"   
     template.save( filename
     )
-    
-    
-    print("Generating Proposal...")
-
     return filename
